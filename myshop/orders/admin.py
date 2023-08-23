3e63a890bc2672ee0f7d96d060c1ffe0cbf997b0
Bonfacegitonga <bonfaceg303@gmail.com>
2023-08-23 11:18:20 +0300

    tailwind

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -2,16 +2,26 @@ from django.contrib import admin
 from .models import Order, OrderItem
 from django.utils.html import format_html
 
+
 # Register your models here.
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    row_id_fields = ['product']
+    row_id_fields = ["product"]
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display =  ['id', 'first_name', 'last_name', 'email',
-                    'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
-    list_filters =  ['paid', 'created', 'updated']
-    inlines =  [OrderItemInline]
-    
+    list_display = [
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "address",
+        "postal_code",
+        "city",
+        "paid",
+        "created",
+        "updated",
+    ]
+    list_filters = ["paid", "created", "updated"]
+    inlines = [OrderItemInline]
